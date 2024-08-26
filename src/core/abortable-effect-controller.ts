c172e9272f917c1f9c08c6cbcc7cd29ff8c422dc
ChenYn <chenyn01@kanyun.com>
2024-08-26 14:24:53 +0800

    maintain controller map with cId

--- src/core/abortable-effect-controller.ts
+++ src/core/abortable-effect-controller.ts
@@ -9,13 +9,32 @@ class AbortableEffectControllerError extends Error {
 }
 
 export class AbortableEffectController {
-    constructor(private readonly _id?: string) {
+    private static _controllerId = 0
+    private static _controllerMap = new Map<string, AbortableEffectController>()
+
+    static globalCleanup() {
+        // 检查是否所有 controller 都被 abort 了，存在没有 abort 的就报错
+        for (const controller of this._controllerMap.values()) {
+            if (!controller.abortSignal.aborted) {
+                throw new AbortableEffectControllerError(
+                    'Controller is not aborted.',
+                    controller.id
+                )
+            }
+        }
+        // 清理所有 controller 持有以释放内存
+        this._controllerMap.clear()
+    }
+
+    private readonly id: string
+    constructor(id_?: string) {
         if (process.env.NODE_ENV !== 'production') {
             console.log(`Create Controller (${this.id})`)
         }
-    }
-    get id() {
-        return this._id ?? '-'
+        // 每个 controller 有一个唯一 id
+        this.id = `${AbortableEffectController._controllerId++}:${id_ ?? '-'}`
+        // 静态变量持有所有 controller 的映射关系
+        AbortableEffectController._controllerMap.set(this.id, this)
     }
 
     private abortController = new AbortController()
@@ -57,24 +76,23 @@ export class AbortableEffectController {
     }
 
     createChildController(id?: string) {
-        // 如果已经被销毁，直接抛出异常，不允许再创建子控制器
+        // 父控制器已经被销毁，直接抛出异常，不允许再创建子控制器
         if (this.abortSignal.aborted) {
             throw new AbortableEffectControllerError(
                 'Cannot create child controller to an already destroyed controller.',
                 this.id
             )
         }
-        const controller = new AbortableEffectController(id)
-
-        // 子控制器会随着复制父控制器的销毁事件
-        this.onCleanup(() => {
-            // 但允许子控制器自行销毁，这里是为了不会因为重复销毁子容器而抛出异常
-            if (controller.abortSignal.aborted) {
-                return
+        const childController = new AbortableEffectController(id)
+        const safeDestroy = () => {
+            // 允许子控制器自行销毁，这里是为了不会因为重复销毁子容器而抛出异常
+            if (!childController.abortSignal.aborted) {
+                childController.destroy()
             }
-            controller.destroy()
-        })
+        }
+        // 子控制器会跟随父控制器一并销毁
+        this.onCleanup(safeDestroy)
 
-        return controller
+        return childController
     }
 }
