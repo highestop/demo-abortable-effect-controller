74f7374f61dbab8a9498b1ab27d14c8e9cd2bffa
ChenYn <highestop@qq.com>
2024-08-28 23:00:27 +0800

    use safe-call in microtask

--- src/interface/queue-microtask.ts
+++ src/interface/queue-microtask.ts
@@ -1,18 +1,11 @@
 import { AbortableEffectController } from '../core/abortable-effect-controller'
+import { safeCall } from './safe-call'
 
 export function queueMicrotaskWithController<T>(
     callback: () => void,
     controller: AbortableEffectController
 ): void {
     queueMicrotask(() => {
-        if (controller.abortSignal.aborted) {
-            if (process.env.NODE_ENV !== 'production') {
-                console.log(
-                    `Microtask is aborted in Controller (${controller.id}).`
-                )
-            }
-            return
-        }
-        callback()
+        safeCall(callback, controller, 'Microtask')
     })
 }
