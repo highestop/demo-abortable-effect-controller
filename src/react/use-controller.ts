ff63134f9453dc4528c7f95166b50f5a8657d8fb
ChenYn <highestop@qq.com>
2024-10-08 22:19:53 +0800

    rename

--- src/react/use-controller.ts
+++ src/react/use-controller.ts
@@ -1,17 +1,17 @@
 import { useEffect } from 'react'
-import { AbortableEffectController } from '../core/abortable-effect-controller'
+import { TracableAbortController } from '../core/tracable-abort-controller'
 
 /**
  *
  * @param setup
  * @param options
  */
-export function useEffectController(
-    setup: (controller: AbortableEffectController) => any,
+export function useController(
+    setup: (controller: TracableAbortController) => any,
     options?: { controllerId?: string; cleanupReason?: string }
 ) {
     useEffect(() => {
-        const controller = new AbortableEffectController(options?.controllerId)
+        const controller = new TracableAbortController(options?.controllerId)
         setup(controller)
         return () => {
             controller.destroy(options?.cleanupReason)
