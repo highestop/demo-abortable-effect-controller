bba0182003972b904887884842d3e488a6e03ba5
ChenYn <highestop@qq.com>
2024-10-08 22:18:34 +0800

    rename impl

--- src/impl/tracable-abort-controller.ts
+++ src/impl/tracable-abort-controller.ts
@@ -1,18 +1,18 @@
 type CleanupCallback = (reason?: string) => void
 
-class AbortableEffectControllerError extends Error {
+class TracableAbortControllerError extends Error {
     constructor(description: string, id?: string) {
         super(description)
-        this.name = 'AbortableEffectControllerError'
+        this.name = 'AbortError'
         this.message = `[${id}] ${description}`
     }
 }
 
-export class AbortableEffectController {
+export class TracableAbortController {
     private static _controllerId = 0
-    private static _controllerMap = new Map<string, AbortableEffectController>()
+    private static _controllerMap = new Map<string, TracableAbortController>()
     static controllerKeys() {
-        return AbortableEffectController._controllerMap.keys()
+        return TracableAbortController._controllerMap.keys()
     }
 
     readonly id: string
@@ -20,33 +20,31 @@ export class AbortableEffectController {
         if (process.env.NODE_ENV !== 'production') {
             console.log(`Create Controller (${this.id})`)
         }
-        // 每个 controller 有一个唯一 id
-        this.id = `${AbortableEffectController._controllerId++}:${id_ ?? '~'}`
-        // 静态变量持有所有 controller 的映射关系
-        if (AbortableEffectController._controllerMap.has(this.id)) {
-            throw new AbortableEffectControllerError(
+        
+        this.id = `${TracableAbortController._controllerId++}:${id_ ?? '-'}`
+        
+        if (TracableAbortController._controllerMap.has(this.id)) {
+            throw new TracableAbortControllerError(
                 'Controller id duplicated',
                 this.id
             )
         }
-        AbortableEffectController._controllerMap.set(this.id, this)
+        TracableAbortController._controllerMap.set(this.id, this)
     }
 
     private _destroyed: { status: false } | { status: true; reason?: string } = { status: false }
     private _destroy(reason?: string) {
         this._destroyed = { status: true, reason }
-        AbortableEffectController._controllerMap.delete(this.id)
+        TracableAbortController._controllerMap.delete(this.id)
     }
     get destroyed() {
         return !!this._destroyed.status
     }
 
-    // 得是个有序的列表，在 cleanup 时要反序要清理
     private cleanupCallbacks: CleanupCallback[] = []
     onCleanup = (cleanupCallback: CleanupCallback) => {
-        // 如果已经被销毁，直接抛出异常，不允许再追加清理工作
         if (this.destroyed) {
-            throw new AbortableEffectControllerError(
+            throw new TracableAbortControllerError(
                 'Cannot attach cleanup callback to an already destroyed controller.',
                 this.id
             )
@@ -55,9 +53,8 @@ export class AbortableEffectController {
     }
 
     destroy(reason?: string) {
-        // 如果已经被销毁，直接抛出异常，只允许销毁一次
         if (this.destroyed) {
-            throw new AbortableEffectControllerError(
+            throw new TracableAbortControllerError(
                 'Cannot destroy an already destroyed controller.',
                 this.id
             )
@@ -67,7 +64,6 @@ export class AbortableEffectController {
             console.log(`Destroy Controller (${this.id})`)
         }
 
-        // 反序执行 cleanupCallbacks 并且清空列表
         while (this.cleanupCallbacks.length) {
             const cleanupCallback = this.cleanupCallbacks.pop()
             cleanupCallback?.(reason)
@@ -77,21 +73,19 @@ export class AbortableEffectController {
     }
 
     createChildController(id?: string) {
-        // 父控制器已经被销毁，直接抛出异常，不允许再创建子控制器
         if (this.destroyed) {
-            throw new AbortableEffectControllerError(
+            throw new TracableAbortControllerError(
                 'Cannot create child controller to an already destroyed controller.',
                 this.id
             )
         }
-        const childController = new AbortableEffectController(id)
+        const childController = new TracableAbortController(id)
         const safeDestroy = () => {
-            // 允许子控制器自行销毁，这里是为了不会因为重复销毁子容器而抛出异常
             if (!childController.destroyed) {
                 childController.destroy()
             }
         }
-        // 子控制器会跟随父控制器一并销毁
+
         this.onCleanup(safeDestroy)
 
         return childController
